09ecaf15183e66ff4341e7b95298e4cd302093ab
theekshani99 <84667815+theekshani99@users.noreply.github.com>
2023-02-03 19:57:40 +0530

    Update logout.php
    fix redirect

--- online-shopping-system/logout.php
+++ online-shopping-system/logout.php
@@ -3,15 +3,17 @@
 session_start();
 
 unset($_SESSION["uid"]);
-
 unset($_SESSION["name"]);
 
-$BackToMyPage = $_SERVER['HTTP_REFERER'];
-if(isset($BackToMyPage)) {
-    header('Location: '.$BackToMyPage);
+$backToMyPage = $_SERVER['HTTP_REFERER'];
+$trustedURLs = array('https://example.com', 'https://example.org');
+
+if (isset($backToMyPage) && in_array($backToMyPage, $trustedURLs)) {
+    header('Location: '.$backToMyPage);
 } else {
     header('Location: index.php'); // default page
 }
-   
+
+exit;
 
 ?>
