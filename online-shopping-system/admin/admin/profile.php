f99c15714ab5fb1e79a71648e0ed939e08b7e8ec
Sachini Shermila <90818907+ShermilaPKSS@users.noreply.github.com>
2023-02-04 10:49:34 +0530

    Update profile.php

--- online-shopping-system/admin/admin/profile.php
+++ online-shopping-system/admin/admin/profile.php
@@ -17,7 +17,7 @@ if (isset($_POST['re_password']))
     {
     if ($new_pass == $re_pass)
       {
-        $pass = md5($re_pass);
+        $pass = password_hash($re_pass, PASSWORD_BCRYPT);
       $update_pwd = mysqli_query($con,"UPDATE admin_info set admin_password='$pass' where admin_id='6'");
       echo "<script>alert('Update Sucessfully'); </script>";
       }
