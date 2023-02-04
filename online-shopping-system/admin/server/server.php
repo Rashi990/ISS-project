6d8e1e4bcd1615dc277aaeef1e99cd144c3cfaf4
Sachini Shermila <90818907+ShermilaPKSS@users.noreply.github.com>
2023-02-04 10:42:08 +0530

    Update server.php

--- online-shopping-system/admin/server/server.php
+++ online-shopping-system/admin/server/server.php
@@ -51,7 +51,7 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-    $password = md5($password_1);//encrypt the password before saving in the database
+    $password = password_hash($password_1, PASSWORD_BCRYPT);//encrypt the password before saving in the database
 
     $query = "INSERT INTO admin_info (admin_name, admin_email, admin_password)
           VALUES('$username', '$email', '$password')";
