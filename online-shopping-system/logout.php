068e06fc5f69a342d43226b09dee0515c207001a
theekshani99 <84667815+theekshani99@users.noreply.github.com>
2023-02-15 01:00:46 +0530

    Update logout.php

--- online-shopping-system/logout.php
+++ online-shopping-system/logout.php
@@ -6,12 +6,12 @@ unset($_SESSION["uid"]);
 unset($_SESSION["name"]);
 
 $backToMyPage = filter_var($backToMyPage, FILTER_VALIDATE_URL);
-$trustedURLs = array('https://example.com', 'https://example.org');
+$trustedURLs = array('index.php ');
 
 if ($backToMyPage && in_array($backToMyPage, $trustedURLs)) {
     header('Location: '.$backToMyPage);
 } else {
-    header('Location: https://example.com');
+    header('Location:index.php ');
 }
 
 exit;
